35a15ea8bbacd255bdf1f25e4bbe0326500f78c5
hugo miranda <hugo.miranda.souza@gmail.com>
2022-10-19 20:33:27 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/bcrypt.adapter.spec.ts
+++ src/infra/cryptography/bcrypt.adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt.adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hash')
+  }
+}))
+
 describe('bcrypt adapter', () => {
   test('should call bcrypt with correct values', async () => {
     const salt = 12
@@ -10,5 +16,14 @@ describe('bcrypt adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+
+    const hash = await sut.encrypt('any_value')
+
+    expect(hash).toBe('hash')
   })
 })

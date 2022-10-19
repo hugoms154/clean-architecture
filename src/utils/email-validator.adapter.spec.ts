c0b34d3102412b42cc4397688cf5f26361e8735f
hugo miranda <hugo.miranda.souza@gmail.com>
2022-10-19 19:11:02 -0300

    refactor: change EmailValidatorAdapter filename

--- src/utils/email-validator.adapter.spec.ts
+++ src/utils/email-validator.adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator-adapter.'
+import { EmailValidatorAdapter } from './email-validator.adapter'
 import validator from 'validator'
 
 interface SutTypes {
